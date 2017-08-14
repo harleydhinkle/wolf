ee015d7d98982bb70366272760bd683963820e21
s179057 <s179057@SEA-8E3EDA>
2017-08-14 12:47:59 -0700

    Add project files.

---
+++ 1.Hello aie/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Hello AIE"<<std::endl;
+	//mixtip
+	std::cout << "its ice to have fun" << std::endl;
+	std::cout << "this bin a punful day" << std::endl;
+	std::cout << "its a bone to have time" << std::endl;
+	std::cout << "have a bony day" << std::endl;
+	std::cout << "i hop im this puns r rib ticuling" << std::endl;
+	std::cout << "im thius puns really hit the funny bone" << std::endl;
+	std::cout << "this is a  bone time" << std::endl;
+	std::cout << "i hop its ice time to night" << std::endl;
+	//conting from zero to 5 and to zero
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	system("pause");
+	//SyntacticalErrors-A
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl;
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+	system("pause");
+	//SyntacticalErrors - b
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+	system("pause");
+}
