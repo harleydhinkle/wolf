fa27df8003953cd5ba6174cbd5609ec9168dfda8
wolfking068 <wolfking068@gmail.com>
2017-08-16 10:26:41 -0700

    4-funching more

--- 4-functions/main.cpp
+++ 4-functions/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cmath>
 void PrintHelloFunctions()
 {
 	std::cout << "Hello Functions" << std::endl;
 }
-int pos2(int a);
+int pos2(int a); 
 float frac(int num, int den);
 
 float LargestDifference(float a, float b, float c);
@@ -33,6 +34,14 @@ void print2(int a , int b)
 		return m;
 	}
  }
+  float distance (float x , float y , float y2 , float x2)
+  {
+	  return sqrtf(((x2 - x)* (x2 - x) + (y2 - y)* (y2 - y)));
+  }
+  float eta(float x, float y ,float x2 ,float y2 ,float unitsPerSec) 
+  {
+	  return distance(x, y, x2, y2) / unitsPerSec;
+  }
   int max(int j, int i) 
   {
 	  if (j>i)
@@ -48,7 +57,7 @@ void print2(int a , int b)
 		  return i;
 	  }
   }
-   int clamp(int min, int max, int c)
+  int clamp(int min, int max, int c)
   {
 	  if (c < min) 
 	  {
@@ -72,23 +81,31 @@ void print2(int a , int b)
 	  }
   }
 
-int main()
-{	
-	PrintHelloFunctions();
-	int a, b;
-
-	std::cout << "Enter two numbers: ";
-	std::cin >> a >> b;
-
-	std::cout << min(a, b)<<std::endl;
-	system("pause");
-	clamp(22,33,55);
-	int d, c;
-	std::cout << "Enter two numbers: ";
-	std::cin >> d >> c;
-	std::cout << max(d, c) << std::endl;
-	print2(4,5);
-	std::cout << sum(3, 6, 7) << std::endl;
-	system("pause");
-		return 0;
-}
+  int main()
+  {
+	  PrintHelloFunctions();
+	  int a, b;
+	  std::cout << "Enter two numbers: ";
+	  std::cin >> a >> b;
+	  std::cout << min(a, b) << std::endl;
+	  system("pause");
+	  int d, c;
+	  std::cout << "Enter two numbers: ";
+	  std::cin >> d >> c;
+	  std::cout << max(d, c) << std::endl;
+	  system("pause");
+	  int f, g, k;
+	  std::cout << "Enter three numbers: ";
+	  std::cin >> f >> g >> k;
+	  std::cout << clamp(f,g,k) << std::endl;
+	  system("pause");
+	  print2(4, 5);
+	  system("pause");
+	  std::cout << sum(3, 6, 7) << std::endl;
+	  system("pause");
+	  std::cout << distance(7, 23, 9, 0) << std::endl;
+	  system("pause");
+	  std::cout << eta(4, 4, 4, 8, 4) << std::endl;
+	  system("pause");
+	  return 0;
+  }
